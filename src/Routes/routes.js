cdc19fc049dc0167c29bd9b0f8b5a70e9860716b
André Rebonato <andrerebonato@users.noreply.github.com>
2020-06-14 12:30:39 -0300

    Adicionado nova formato para o routes

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import UserController from '../Controllers/UserController';
+import { configs } from '../Configs/configs';
 
 const routes = Router();
 
-routes.get('/users-get-all', UserController.getAll);
-
+routes.get(configs.routes.user.getAll, UserController.getAll);
 
 export default routes;
