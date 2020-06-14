4c2e55fa1456d7ea778226e50ce8a4142c1b47b1
André Rebonato <andrerebonato@users.noreply.github.com>
2020-06-14 13:18:56 -0300

    Refatoração nos imports

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -1,8 +1,7 @@
-import ResponseType from './ResponseType';
+import ResponseType from '../Helpers/ResponseType';
 import { Request, Response } from 'express';
 
 export default class UserController {
-
     public static async getAll(req: Request, res: Response): Promise<any> {
         //adicionar logica de requisição do banco de dados aqui.
         return res.status(200).json(ResponseType.success('Carregado todos os usuários', []));
