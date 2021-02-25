e54d6605ceaeb341255bc3885bdc81a00666d42a
André Rebonato <andrerebonato913@Hotmail.com>
2021-02-25 10:45:59 -0300

    create verify token method

--- src/Services/Auth/AuthJwtService.ts
+++ src/Services/Auth/AuthJwtService.ts
@@ -1,16 +1,17 @@
 import jwt from 'jsonwebtoken';
 import path from "path";
 
+import IUser from "../../Types/User";
 import logger from "../Log/LogErrorService";
 import { configs } from '../../Configs/configs';
 
-export class AuthJwtService {
+export default class AuthJwtService {
     public static classPath: string = path.basename(__filename);
 
-    public static generateToken(userData: Object){
+    public static generateToken(userData: IUser){
         try {
             logger.dispatch(AuthJwtService.classPath, logger.types.info, "Started generateToken");
-            const token: string = jwt.sign(userData, configs.authJwt.secretKey);
+            const token: string = jwt.sign(String(userData._id), configs.authJwt.secretKey);
             if(token) {
                 return token;
             } else {
@@ -23,10 +24,20 @@ export class AuthJwtService {
             throw err;
         }
     }
-    public static verifyToken(token: string){
+
+    public static verifyToken(req, res, next){
         try{
             logger.dispatch(AuthJwtService.classPath, logger.types.info, "Started verifyToken.");
-            return jwt.verify(token, configs.authJwt.secretKey);
+            const token = req.headers.authorization?.trim().split("Bearer")[1].replace(" ", "");
+            jwt.verify(token, configs.authJwt.secretKey, function (err, decoded) {
+                if (err) {
+                    return res.status(500).send({ auth: false, message: 'Token inválido.' }); 
+                }
+                req.user = decoded; 
+                console.log("User Id: " + decoded)
+                next();
+            });
+            
         }catch(err){
             logger.dispatch(AuthJwtService.classPath, logger.types.error, `Exception: ${String(err)}`);
             throw err;
