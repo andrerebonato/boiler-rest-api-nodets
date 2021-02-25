68c2957e08dcd8d24ee787bc3c2bb549bd011489
André Rebonato <andrerebonato913@Hotmail.com>
2021-02-25 10:44:05 -0300

    Create http service due express

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import path from "path";
+import { createServer } from "http";
 
 import app from './app';
 import logger from "./Services/Log/LogErrorService";
 
 let classPath: string = path.basename(__filename);
 
-const port = 3333;
+const http = createServer(app);
+const port = app.get("port");
+
+http.listen(port);
 logger.dispatch(classPath, logger.types.info, `Starting service at port: ${port}`);
 
-app.listen(port);
 logger.dispatch(classPath, logger.types.info, `Service stated at port: ${port}`);
 
