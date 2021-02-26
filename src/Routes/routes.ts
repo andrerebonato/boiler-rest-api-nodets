49e7bdd490b3ceccf1c1135fd04aae2dc02d1a68
André Rebonato <andrerebonato913@hotmail.com>
2021-02-26 20:12:05 -0300

    Initialized swagger

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -7,12 +7,28 @@ import { configs } from '../Configs/configs';
 import "moment/locale/pt-br";
 
 const router = express.Router();
-
+/**
+ * @swagger
+ * !Needs authenticate
+ * /api/v1/user/get-all
+ *  get:
+ *      description: get all active users
+*       responses:
+ *          200:
+ *              description: Success
+ * 
+ * 
+ */
 router.get(configs.routes.user.getAll, AuthJwtService.verifyToken, UserController.getAll);
+
 router.get(configs.routes.user.getById, AuthJwtService.verifyToken, UserController.getById);
+
 router.get(configs.routes.user.me, AuthJwtService.verifyToken, UserController.me);
+
 router.post(configs.routes.user.create, AuthJwtService.verifyToken, UserController.create);
+
 router.post(configs.routes.user.refreshToken, UserController.refreshToken);
+
 router.post(configs.routes.user.authenticate, UserController.authenticate);
 
 export default router;
