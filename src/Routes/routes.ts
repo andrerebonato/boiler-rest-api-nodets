442f0bef34bf0759bbbb78cd176fbc8db5631d86
André Rebonato <andrerebonato913@hotmail.com>
2021-02-26 21:31:51 -0300

    Improvements
    Added delete route

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -20,15 +20,11 @@ const router = express.Router();
  * 
  */
 router.get(configs.routes.user.getAll, AuthJwtService.verifyToken, UserController.getAll);
-
 router.get(configs.routes.user.getById, AuthJwtService.verifyToken, UserController.getById);
-
 router.get(configs.routes.user.me, AuthJwtService.verifyToken, UserController.me);
-
 router.post(configs.routes.user.create, AuthJwtService.verifyToken, UserController.create);
-
 router.post(configs.routes.user.refreshToken, UserController.refreshToken);
-
 router.post(configs.routes.user.authenticate, UserController.authenticate);
+router.delete(configs.routes.user.delete, AuthJwtService.verifyToken, UserController.delete);
 
 export default router;
